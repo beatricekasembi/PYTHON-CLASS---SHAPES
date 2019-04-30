8a36271ad7468f56aac84fbd78df8b9263dc97bd
Beatrice Kasembi <47854144+beatricekasembi@users.noreply.github.com>
2019-04-30 19:42:28 +0300

    Add files via upload
    SHAPES - PYTHON CLASS

---
+++ shapes.py
@@ -0,0 +1,96 @@
+class circle:
+	def __init__(self,radius):
+		
+		self.area = 0
+		self.r = 0
+		
+	
+	def area(self):
+		self.area = (3.142*r*2)
+		for a in self.area:
+			return area
+
+	def calculate_circumference(self):
+		circumference = (2*3.140*r)
+
+		for circumference in self.calculate_circumference :
+			return circumference
+
+
+
+
+
+
+
+
+class square:
+	def __init__(self,a):
+
+		self.perimeter = perimeter
+
+	def calculate_perimeter(self,p):
+
+	self.perimeter = p(4*a)
+	for p in perimeter:
+		return perimeter
+
+
+
+
+
+
+class rectangle:
+	def __init__(self,w,l):
+
+		self.perimeter = perimeter
+
+		self.area = 0
+	
+
+	def rectangle_area(self,rec):
+	self.rectangle_area = rec(w*l)
+	for area in rectangle_area:
+		return rectangle_area
+
+	def rectangle_perimeter(self,per):
+	self.rectangle_perimeter = per(2(l+w))
+	for perimeter in rectangle_perimeter:
+		return rectangle_perimeter
+
+
+
+
+
+
+
+
+class sphere:
+	def __init__(self,r):
+
+		self.volume = volume 
+		self.area = 0
+
+	def sphere_area(self,sph):
+	self.sphere_area = sph(4*3.142*r*2)
+	for area in self.sphere_area:
+		return sphere_area
+
+	def sphere_volume(self,vol):
+	self.sphere_volume = vol(4/3(3.142*r*3))
+	for volume in self.sphere_volume:
+		return sphere_volume
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
